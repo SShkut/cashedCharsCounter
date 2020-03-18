eae38f00089dc7102d2279796e906c06a92f68b8
sshkut <sgshkut@gmail.com>
2020-03-18 17:21:58 +0100

    Correct test case.

--- src/test/java/com/foxminded/collectionframework/CachedCharsCounterTest.java
+++ src/test/java/com/foxminded/collectionframework/CachedCharsCounterTest.java
@@ -3,7 +3,11 @@ package com.foxminded.collectionframework;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -35,7 +39,20 @@ class CachedCharsCounterTest {
 	
 	@Test
 	void givenText_whenCount_thenCorrectResult() {
-		String text = "HellO World  !";
+		String text = "HellO World  !";		
+		Map<Character, Long> expected = new LinkedHashMap<>();
+		expected.put('H', 1L);
+		expected.put('e', 1L);
+		expected.put('l', 3L);
+		expected.put('O', 1L);
+		expected.put(' ', 3L);
+		expected.put('W', 1L);
+		expected.put('o', 1L);
+		expected.put('r', 1L);
+		expected.put('d', 1L);		
+		
+		when(charsCounter.count(text)).thenReturn(expected);
+		
 		assertEquals(charsCounter.count(text), cache.count(text));
 	}
 }
